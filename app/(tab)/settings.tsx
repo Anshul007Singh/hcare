5b075c0738d0fb59b8c3c6ecee2c2a989da3e5b0
Anshul Singh <anshuls481@gmail.com>
2024-12-29 16:10:39 +0530

    Initial commit

--- app/(tab)/settings.tsx
+++ app/(tab)/settings.tsx
@@ -8,7 +8,7 @@ export default function Settings() {
       <Card>
     <Card.Title title="Card Title" subtitle="Card Subtitle"  />
     <Card.Content>
-      <Text >Card titlhhhhe</Text>
+      <Text >Card tie</Text>
       <Text >Card content</Text>
     </Card.Content>
     <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
